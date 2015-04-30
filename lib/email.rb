cafd92e87e1c8f28fc44101c3d729107eec80336
Jim Patt & Alex Caste <student@epicodus.com>
2015-04-30 09:50:01 -0700

    Passing spec to add email

--- lib/email.rb
+++ lib/email.rb
@@ -1,9 +1,19 @@
 class Email
 
+  @@emails = []
+
   attr_reader(:e_address)
 
   define_method(:initialize) do |e_address|
     @e_address = e_address
+  end
+
+  define_singleton_method(:all) do
+    @@emails
+  end
+
+  define_method(:save) do
+    @@emails.push(self)
   end
 
 end
