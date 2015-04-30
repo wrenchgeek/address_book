a53c9b38fb68bc5aa5c8ed763dda071e297f16bc
Jim Patt & Alex Caste <student@epicodus.com>
2015-04-30 09:49:17 -0700

    Writing spec to test saving emails

--- spec/address_spec.rb
+++ spec/address_spec.rb
@@ -13,4 +13,12 @@ describe(Email) do
       expect(test_email.e_address()).to(eq("joe@joe.com"))
     end
   end
+
+  describe('#save') do
+    it("it saves the email") do
+      test_email = Email.new("joe@joe.com")
+      test_email.save()
+      expect(Email.all()).to(eq([test_email]))
+    end
+  end
 end
